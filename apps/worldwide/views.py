22f6bdbc40c3364ecf6a91c324bf952901b93632
yc <do36421@naver.com>
2024-12-09 17:55:26 +0900

    Style:레이아웃 css잡는중
    Fix:폴리움 전역css적용 오류해결
    -html을변환후 넣어줬는데 html통으로 넣는 코드로 바꿈

--- apps/worldwide/views.py
+++ apps/worldwide/views.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template
+import folium
+from io import BytesIO
 
 worldwide_bp = Blueprint(
     'worldwide',
@@ -10,4 +12,17 @@ worldwide_bp = Blueprint(
 
 @worldwide_bp.route('/')
 def worldwide_data():
-    return render_template('worldwide/worldwide_data.html')
+     # Folium 지도 생성
+    start_coords = [20, 0]  # 전 세계 중심 좌표
+    world_map = folium.Map(location=start_coords, zoom_start=2)
+
+    # 마커 추가
+    folium.Marker([37.5665, 126.9780], popup='Seoul, South Korea').add_to(world_map)  # 서울
+    folium.Marker([40.7128, -74.0060], popup='New York, USA').add_to(world_map)      # 뉴욕
+    folium.Marker([48.8566, 2.3522], popup='Paris, France').add_to(world_map)        # 파리
+
+    # 지도 HTML을 문자열로 변환
+
+    map_html = world_map._repr_html_()
+
+    return render_template('worldwide/worldwide_data.html', map_html=map_html)
