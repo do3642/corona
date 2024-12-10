e8607ab002a693e856c091ce1b8d47155cb56f3c
yeong <chlgkdud111@gmail.com>
2024-12-10 18:38:40 +0900

    Add: base.html 만듦. Update: 그래프 보관을 위해 분리함(진행중)

--- apps/domestic/views.py
+++ apps/domestic/views.py
@@ -1,8 +1,15 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 import folium
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
+
+from apps.domestic.graphs.graph_index import generate_garph
+
+import matplotlib
+matplotlib.use('Agg')
 
 bp = Blueprint(
   "domestic",
@@ -11,19 +18,19 @@ bp = Blueprint(
   static_folder="static"
   )
 
-# 국내 코로나 현황 데이터 불러옴.
-path = os.path.join('apps', 'static', 'data', '국내_코로나_발생_현황.xlsx')
-covid_data = pd.read_excel(path, sheet_name=None)
-
-for sheet,a in covid_data.items():
-  print(f'이름 : {sheet}')
-  print(a)
-
 @bp.route('/')
 def index():
   # 지도가 전국이 다 보일 수 있도록 설정.
   map = folium.Map( location=[36, 128.00025], zoom_start=7.25, tiles="CartoDB positron")
-
+  
   map_html = map._repr_html_()
-  print(covid_data.head)
+
   return render_template('domestic/index.html', map_html = map_html)
+
+@bp.route('/graph/<graph_id>')
+def get_graph(graph_id):
+  graph_base64 = generate_garph(graph_id)
+  if graph_base64:
+    return jsonify({'graph' : graph_base64})
+  else:
+    return jsonify({'error' : '해당 id를 찾지 못했습니다.'}), 404
