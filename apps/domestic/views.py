b53c5a1a9e20928420a28b87174e12cd42e80cbe
yeong <chlgkdud111@gmail.com>
2024-12-11 12:24:25 +0900

    Remove : 그래프를 js를 통해 만들기 위해 그 전에 작업한 거 다 지움. Add : chart.js 사용을 위해 html에 필요한 것들 추가 ;

--- apps/domestic/views.py
+++ apps/domestic/views.py
@@ -1,15 +1,5 @@
-from flask import Blueprint, render_template, jsonify
+from flask import Blueprint, render_template
 import folium
-import os
-import pandas as pd
-import matplotlib.pyplot as plt
-from io import BytesIO
-import base64
-
-from apps.domestic.graphs.graph_index import generate_garph
-
-import matplotlib
-matplotlib.use('Agg')
 
 bp = Blueprint(
   "domestic",
@@ -26,11 +16,3 @@ def index():
   map_html = map._repr_html_()
 
   return render_template('domestic/index.html', map_html = map_html)
-
-@bp.route('/graph/<graph_id>')
-def get_graph(graph_id):
-  graph_base64 = generate_garph(graph_id)
-  if graph_base64:
-    return jsonify({'graph' : graph_base64})
-  else:
-    return jsonify({'error' : '해당 id를 찾지 못했습니다.'}), 404
