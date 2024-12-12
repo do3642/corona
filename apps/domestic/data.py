3595b9c7538b312b39a4f6adcd97356e6057897a
yeong <chlgkdud111@gmail.com>
2024-12-12 17:07:40 +0900

    Add: 버튼 누르면 그래프 바뀌게 하는 기능 추가

--- apps/domestic/data.py
+++ apps/domestic/data.py
@@ -8,19 +8,43 @@ bp = Blueprint('data', __name__)
 path = os.path.join('apps','static','data','국내_코로나_발생_현황.xlsx')
 covid_data = pd.read_excel(path, sheet_name=None)
 
+p_path = os.path.join('apps','static', 'data', '연령별_인구수.xlsx')
+population_data = pd.read_excel(p_path)
+
 # 엑셀에 있는 시트들이 딕셔너리 안에 하나씩 들어감.
 sheet_data = { sheet : data for sheet, data in covid_data.items() }
 
-@bp.route('/area_incidence', methods=['GET'])
-def get_area_incidence():
-  area = sheet_data.get('시군구별(발생률,사망률)')
-  area_total = area.query("시군구=='합계'")
-  data = {
-    "labels": area_total['시도명'].tolist(),
-    "values": area_total['발생률\n(인구10만명당, 명)'].tolist(),
-    "label": '지역별 코로나 발생률(23.8.31 0시기준(인구 10만명당, 명))',
-    "type": 'line',
-    "borderColor": 'rgba(101,148,189,1)'
-  }
+@bp.route('/<graph_id>', methods=['GET'])
+def get_graph(graph_id):
+  if graph_id == 'area_incidence':
+    area = sheet_data.get('시군구별(발생률,사망률)')
+    area_total = area.query("시군구=='합계'")
+    data = {
+      "labels": area_total['시도명'].tolist(),
+      "values": area_total['발생률\n(인구10만명당, 명)'].tolist(),
+      "label": '지역별 코로나 발생률(인구 10만명당, 명)',
+      "type": "bar",
+      "backgroundColor": "rgba(101,148,189,1)"
+    }
+    return jsonify(data)
+  
+  elif graph_id == 'area_death':
+    area = sheet_data.get('시군구별(발생률,사망률)')
+    area_total = area.query("시군구=='합계'")
+    data = {
+      'labels': area_total['시도명'].tolist(),
+      'values': area_total['사망률\n(인구10만명당, 명)'].tolist(),
+      'label': '지역별 코로나 사망률(인구 10만명당, 명)',
+      'type': 'bar',
+      'backgroundColor': 'rgba(189,101,101,1)'
+    }
 
-  return jsonify(data)
+    return jsonify(data)
+  
+  elif graph_id == 'age_incidence':
+    age = sheet_data.get('연령별(10세단위)')
+    cumulative_cases = age.iloc[0,2:]
+
+  
+  else:
+    return jsonify({"error": "해당 그래프ID는 존재하지 않는 그래프입니다."}), 404
--- apps/domestic/static/js/graph.js
+++ apps/domestic/static/js/graph.js
@@ -1,44 +1,58 @@
+// data.py에 불러온 데이터들을 반복을 돌리면서 createChart 함수에 넣어줌.
+let chartInstance=null;
 document.addEventListener("DOMContentLoaded", () => {
-  const graphConfigs = [
-    { id: 'chart-area-incidence', url: '/api/graph/area_incidence' },
-  ];
 
-  graphConfigs.forEach(config => {
-    fetch(config.url)
-      .then(response => response.json())
-      .then(data => {
-        if (data.error) {
-          console.error(`Error loading graph: ${config.url}`, data.error);
-          document.getElementById(config.id).innerHTML = `<p>데이터 처리 중에 오류가 발생했습니다.</p>`;
-        } else {
-          createChart(config.id, data);
-        }
-      })
-      .catch(error => {
-        console.error(`Fetch error for ${config.url}`, error);
-        document.getElementById(config.id).innerHTML = `<p>서버에서 응답받을 받는데 실패했습니다.</p>`;
-      });
-  });
+  document.querySelector("#graph1 div").addEventListener("click", (event) => {
+    const canvasId = "chart-area"
+    const button = event.target;
+
+    changeChart(canvasId, button);
+  })
+
+
+  
 });
 
+function changeChart(canvasId, button) {
+  const graphId = button.getAttribute("data-graph");
+
+  if (graphId) {
+    fetch(`/api/graph/${graphId}`)
+      .then(response => response.json())
+      .then(data => {
+        if (chartInstance) {
+          chartInstance.destroy();
+        }
+        createChart(canvasId, data);
+      })
+      .catch(error => console.error(`Error loading graph: ${graphId}`, error));
+  }
+}
+
+// 위에서 받은 데이터를 받아 데이터마다 다른 그래프가 나올 수 있도록 함.
 function createChart(canvasId, data) {
   const ctx = document.getElementById(canvasId).getContext("2d");
-  new Chart(ctx, {
+  chartInstance = new Chart(ctx, {
     type: data.type,
     data: {
       labels: data.labels,
       datasets: [{
         label: data.label,
         data: data.values,
-        borderColor: data.borderColor,
+        backgroundColor: data.backgroundColor,
         borderWidth: 1
       }]
     },
     options: {
-      responsive: false,
+      responsive: true,
       scales: {
         y: {
           beginAtzero:true
+        }
+      },
+      layout: {
+        padding:{
+          top: 90
         }
       }
     }
